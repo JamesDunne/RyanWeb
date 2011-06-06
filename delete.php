0b887297e417c3a1ac049d3c3da59255328d5dd7
James Dunne <james.jdunne@gmail.com>
2011-06-06 13:50:22 -0500

    Fixed delete.php to use string concat.

--- delete.php
+++ delete.php
@@ -4,15 +4,15 @@ header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 
 $filename = $_POST['filename'];
-if ($filename == null)
+if (!isset($filename))
 {
     $js = array('success' => false);
     echo json_encode($js);
     return;
 }
 
-error_log('/var/www/ryan/pics/' + $filename);
-unlink('/var/www/ryan/pics/' + $filename);
+error_log('/var/www/ryan/pics/' . $filename);
+unlink('/var/www/ryan/pics/' . $filename);
 
 $js = array('success' => true);
 echo json_encode($js);
